5e9d0bd4b5216cd1f82c28b9db7d31b1ccbb66f0
AQUINO R. D. G <aquinordga@gmail.com>
2022-08-23 17:18:33 -0300

    new_free_catbird

--- src/rdga_4k/__init__.py
+++ src/rdga_4k/__init__.py
@@ -74,7 +74,6 @@ def catbird(m, n, k, lmbd=.5, eps=.5, random_state=None):
     y = np.array(y, dtype=np.int64)
 
     return X, y
-<<<<<<< HEAD
     
 def free_catbird(n, rate, feat_sig, lmbd=.5, eps=.5, random_state=None):
 
@@ -147,25 +146,69 @@ def free_catbird(n, rate, feat_sig, lmbd=.5, eps=.5, random_state=None):
     
     return X, y
     
-    ### NOVA VERSÃO ###
-#    
-#        for i in range(len(rate)):
-#        W = random_state.normal(0, 1, (feat_sig[i], feat_sig[i]))
-#        idx = list(random_state.choice(list(range(n)), size=feat_sig[i], replace=False))
-#        
-#        q += 1
-#        for j in range(rate[i]):    
-#            A = [random_state.normal(0, 1, feat_sig[i])]
-#            A_W = [[sum(a*b for a,b in zip(A_row,W_col)) for W_col in zip(*W)] for A_row in A]            
-#            A_W_sig = sig_f(A_W[0])
-#            
-#            A_W_sig_bin = np.array([random_state.binomial(1, eps) for i in range(n)])
-#            A_W_sig_bin[idx] = binarize(A_W_sig, lmbd)
-#
-#            y.append(q)
-#            X.append(A_W_sig_bin)
-#
-#    X = np.array(X, dtype=np.int64)
-#    y = np.array(y, dtype=np.int64)
-#    
-#    return X, y
+def new_free_catbird(n, rate, feat_sig, lmbd=.5, eps=.5, random_state=None):
+
+    # libraries
+    import numpy as np
+    import math
+    from numpy.random import RandomState
+
+    if random_state is None:
+        random_state = RandomState()
+    elif type(random_state) == int:
+        random_state = RandomState(random_state)
+    else:
+        assert type(random_state) == RandomState
+
+    # checking
+    assert type(n) == int and n > 1
+
+    assert isinstance(rate, list)
+    
+    assert isinstance(feat_sig, list) and max(feat_sig) <= n
+
+    assert type(lmbd) == float and (lmbd >= 0.0 and lmbd <= 1.0)
+
+    assert type(eps) == float and (eps >= 0.0 and eps <= 1.0)
+
+    # tools
+    def sigmoid(x):
+        return 1 / (1 + math.exp(-x))
+
+    def sig_f(x):
+        return [sigmoid(i) for i in x]
+
+    def binarize(x, lmbd):
+        xbin = []
+        for i in range(len(x)):
+            if x[i] < lmbd:
+                xbin.append(1)
+            else:
+                xbin.append(0)
+        return xbin
+
+    # initialization
+    X = []
+    y = []
+    q = -1
+
+        for i in range(len(rate)):
+        W = random_state.normal(0, 1, (feat_sig[i], feat_sig[i]))
+        idx = list(random_state.choice(list(range(n)), size=feat_sig[i], replace=False))
+        
+        q += 1
+        for j in range(rate[i]):    
+            A = [random_state.normal(0, 1, feat_sig[i])]
+            A_W = [[sum(a*b for a,b in zip(A_row,W_col)) for W_col in zip(*W)] for A_row in A]            
+            A_W_sig = sig_f(A_W[0])
+            
+            A_W_sig_bin = np.array([random_state.binomial(1, eps) for i in range(n)])
+            A_W_sig_bin[idx] = binarize(A_W_sig, lmbd)
+
+            y.append(q)
+            X.append(A_W_sig_bin)
+
+    X = np.array(X, dtype=np.int64)
+    y = np.array(y, dtype=np.int64)
+    
+    return X, y
