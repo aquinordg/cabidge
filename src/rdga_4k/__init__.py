9972fffd9a44e65d77a1134577de88bf6bc8d3f8
AQUINO R. D. G <aquinordga@gmail.com>
2022-09-28 20:53:46 -0300

    create canard
    and reduce_nominal removed

--- src/rdga_4k/__init__.py
+++ src/rdga_4k/__init__.py
@@ -1,9 +1,19 @@
 import numpy as np
+import pandas as pd
 import math
 from numpy.random import RandomState
 from scipy.stats import norm
 
-def catbird(n, rate, feat_sig, lmbd=.8, eps=.2, random_state=None):
+def catbird(n_feat, feat_sig, rate, lmbd=.8, eps=.2, random_state=None):
+    """
+    Parameters:
+    `n_feat` int > 1: number of features
+    `feat_sig` list: number of significant features (no noise), equal to the size of the rate list 
+    `rate` list: division of examples into clusters, equal to the size of the feat_sig list
+    `lmbd` float 0,1: intersection factor between features
+    `eps` float 0,1: feature noise rate
+    `random_state` int: random seed
+    """
 
     if random_state is None:
         random_state = RandomState()
@@ -13,11 +23,11 @@ def catbird(n, rate, feat_sig, lmbd=.8, eps=.2, random_state=None):
         assert type(random_state) == RandomState
 
     # checking
-    assert type(n) == int and n > 1
+    assert type(n_feat) == int and n_feat > 1
 
     assert isinstance(rate, list)
 
-    assert isinstance(feat_sig, list) and max(feat_sig) <= n
+    assert isinstance(feat_sig, list) and max(feat_sig) <= n_feat
 
     assert type(lmbd) == float and (lmbd >= 0.0 and lmbd <= 1.0)
 
@@ -41,7 +51,7 @@ def catbird(n, rate, feat_sig, lmbd=.8, eps=.2, random_state=None):
             A_W = A @ W
             A_W_norm = norm.cdf(A_W / math.sqrt(feat_sig[i]))
 
-            result = random_state.binomial(1, eps, n)
+            result = random_state.binomial(1, eps, n_feat)
             result[idx] = discretize(A_W_norm, lmbd)
 
             y.append(q)
@@ -51,21 +61,59 @@ def catbird(n, rate, feat_sig, lmbd=.8, eps=.2, random_state=None):
     y = np.array(y, dtype=np.int64)
 
     return X, y
-
-
-def reduce_nominal(X, n):
+    
+def canard(n_feat, n_cat, rate, lmbd=10, eps=.3, random_state=None):
     """
     Parameters:
-    `X`: output X from the catbird function
-    `n`: number of resulting nominal features
+    `n_feat` int > 1: number of features
+    `n_cat` int > 1: number of categories
+    `rate` list: division of examples into clusters
+    `lmbd` int >= 1: intersection factor between features
+    `eps` float 0,1: feature noise rate
+    `random_state` int: random seed
     """
+    
+    if random_state is None:
+        random_state = RandomState()
+    elif type(random_state) == int:
+        random_state = RandomState(random_state)
+    else:
+        assert type(random_state) == RandomState
+        
+    # checking
+    assert type(n_feat) == int and n_feat > 1
+    
+    assert type(n_cat) == int and n_cat > 1
 
-    assert X.shape[1] % n == 0
+    assert isinstance(rate, list)
 
-    shift = X.shape[1] // n
-    Xnew = np.zeros(shape=(X.shape[0], n))
+    assert type(lmbd) == int and lmbd >= 1 
 
-    for i in range(n):
-        Xnew[:, i] = X[:, i*shift:(i*shift + shift)].sum(axis=1)
+    assert type(eps) == float and (eps >= 0.0 and eps <= 1.0)
 
-    return Xnew
+    # initialization
+    X = []
+    y = []
+    q = -1
+    cats = list(range(n_cat))
+    bins = [i / n_cat for i in range(0, (n_cat+1))]
+
+    for m in rate:
+        q += 1
+        a = n_feat*[1]
+        b = n_feat*[1]
+        idx = list(random_state.choice(int(n_feat), size=int(n_feat*(1-eps)), replace=False))
+
+        for i in idx:
+            a[i] = 1 + lmbd * random_state.random_sample()
+            b[i] = 1 + lmbd * random_state.random_sample()
+
+        for _ in range(m):
+            ex = np.array([ pd.cut(random_state.beta(a[j], b[j], 1), bins = bins, labels=cats)[0] for j in range(n_feat) ])
+            X.append(ex)
+            y.append(q)
+            
+    X = np.array(X, dtype=np.int64)
+    y = np.array(y, dtype=np.int64)
+            
+    return X, y
